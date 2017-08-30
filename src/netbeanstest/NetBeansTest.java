787b940fbe4a027ee67bd4bb153b653bfa0059eb
gebruiker <gebruiker@DESKTOP-0S94UI1>
2017-08-30 11:19:42 +0200

    Initialisation commit.

---
+++ src/netbeanstest/NetBeansTest.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package netbeanstest;
+
+/**
+ *
+ * @author gebruiker
+ */
+public class NetBeansTest {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int fgtAmount = 0;
+        System.out.println("There are currently " + fgtAmount + " fgts around.");
+        
+        System.out.println("Amount of fgts: " + fgtAmount);
+        skerm sk = new skerm();
+        sk.setVisible(true);
+    }
+    
+}
